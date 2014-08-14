6e5b9fdb3d238a24afb0d64879f2afdf49ae6360
Giordon Stark <kratsg@gmail.com>
2014-08-13 21:18:29 -0500

    Moved, made the file structure smaller, and rewrote quickly.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from offline_jets import offline_jets as OfflineJets
-from gTowers import gFEX_jets as gTowers
-from analysis.analysis import Analysis
-from file_helper.file_helper import RootFile
+import offline_jets as OfflineJets
+import gFEX_jets as gTowers
+#from analysis.analysis import Analysis
+#from file_helper.file_helper import RootFile
